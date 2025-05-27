8c71aba367c33669481ac6b3f0230dd284771619
Hamredahl <JHamredahl@gmail.com>
2025-05-27 13:54:22 +0200

    Added Testclasses to Application and Web.
    Co-authored-by: Nowaxial<90767515+Nowaxial@users.noreply.github.com>

---
+++ EmployeeApp.Application.Tests/EmployeeServiceTests.cs
@@ -0,0 +1,87 @@
+﻿using EmployeesApp.Application.Employees;
+using EmployeesApp.Application.Employees.Interfaces;
+using EmployeesApp.Application.Employees.Services;
+using EmployeesApp.Domain.Entities;
+using Moq;
+using System.Xml.Linq;
+
+namespace EmployeesApp.Application.Tests;
+
+public class EmployeesServiceTests
+{
+    [Fact]
+    public void Add_VerifyAddedEmployee()
+    {
+        //Arrange
+        var employeeRepository = new Mock<IEmployeeRepository>();
+        var employeeService = new EmployeeService(employeeRepository.Object);
+        var testEmployee = new Employee { Id = 1, Name = "Mock Mock", Email = "mock@mock.com" };
+         
+        //Act
+        employeeService.Add(testEmployee);
+
+        //Assert
+        employeeRepository.Verify(o => o.Add(testEmployee), Times.Exactly(1));
+    }
+
+
+    [Fact]
+    public void GetAll_ReturnsEmployeeArray()
+    {
+        //Arrange
+        var employeeRepository = new Mock<IEmployeeRepository>();
+        
+        employeeRepository
+            .Setup(e => e.GetAll())
+            .Returns([
+                new Employee { Email = "mock@mock.com", Id = 1, Name = "Mock Mock" },
+                new Employee { Email = "mock2@mock.com", Id = 2, Name = "Mocke Mocke" }]);
+        var employeeService = new EmployeeService(employeeRepository.Object);
+
+        //Act
+        var result = employeeService.GetAll();
+
+        //Assert
+        Assert.Equal(2, result.Length);
+        Assert.IsType<Employee[]>(result);
+    }
+
+    [Fact]
+    public void GetById_InvalidId_ThrowsException()
+    {
+        //Arrange
+        var employeeRepository = new Mock<IEmployeeRepository>();
+
+        employeeRepository
+            .Setup(e => e.GetById(-1))
+            .Throws<ArgumentException>();
+        var employeeService = new EmployeeService(employeeRepository.Object);
+
+        //Act
+        var result = Record.Exception(() => employeeService.GetById(-1));
+
+        //Assert
+        Assert.IsType<ArgumentException>(result);
+    }
+
+
+    [Fact]
+    public void GetById_ValidID_ReturnsEmployee()
+    {
+        //Arrange
+        var employeeRepository = new Mock<IEmployeeRepository>();
+
+        employeeRepository
+            .Setup(e => e.GetById(1))
+            .Returns(new Employee { Id = 1, Name = "Mock Mock", Email = "mock@mock.com"});
+        var employeeService = new EmployeeService(employeeRepository.Object);
+
+        //Act
+        var result = employeeService.GetById(1);
+
+        //Assert
+        Assert.NotNull(result);
+        Assert.IsType<Employee>(result);
+        employeeRepository.Verify(e => e.GetById(1), Times.Exactly(1));
+    }
+}
---
+++ EmployeesApp.Web.Tests/EmployeesControllerTests.cs
@@ -0,0 +1,69 @@
+﻿using EmployeesApp.Application.Employees.Interfaces;
+using EmployeesApp.Domain.Entities;
+using EmployeesApp.Web.Controllers;
+using EmployeesApp.Web.Views.Employees;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace EmployeesApp.Web.Tests;
+
+public class EmployeesControllerTests
+{
+    [Fact]
+    public void IndexTest()
+    {
+        //Arrange
+        var employeeService = new Mock<IEmployeeService>();
+        employeeService
+            .Setup(o => o.GetAll())
+            .Returns([
+                new Employee { Id = 1, Email = "mock@mock.com", Name = "Mock Mock" },
+                new Employee { Id = 2, Email = "mock2@mock.com", Name = "Mocke Mocke" }]);
+        var controller = new EmployeesController(employeeService.Object);
+
+        //Act
+        var result = controller.Index();
+
+        //Assert
+        Assert.IsType<ViewResult>(result);
+    }
+
+    [Fact]
+    public void DetailsTest()
+    {
+        var employeeService = new Mock<IEmployeeService>();
+        employeeService
+            .Setup(o => o.GetById(1))
+            .Returns(new Employee { Id = 1, Email = "mock@mock.com", Name = "Mock Mock" });
+        var controller = new EmployeesController(employeeService.Object);
+
+        //Act
+        var result = controller.Details(1);
+
+        //Assert
+        Assert.IsType<ViewResult>(result);
+    }
+
+    [Fact]
+    public void CreateTest_NotValid()
+    {
+        //Arrange
+        var model = new CreateVM
+        {
+            Name = "Mock Mock",
+            Email = "mock@mock.com",
+            BotCheck = 2
+        };
+
+        var context = new ValidationContext(model);
+        var results = new List<ValidationResult>();
+
+        //Act
+        var isValid = Validator.TryValidateObject(model, context, results, validateAllProperties: true);
+
+        //Assert
+        Assert.False(isValid);
+    }
+}
